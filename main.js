4942a87d4eb9344186bcba5195661e1884483c21
Saharsh0 <113522933+Saharsh0@users.noreply.github.com>
2023-10-23 17:27:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 video = "";
+object_status = ""
+objects = []
 
 function preload()
 {
@@ -15,6 +17,23 @@ function setup()
 function draw()
 {
     image(video, 0, 0, 480, 380);
+    if(object_status != "")
+    {
+        objectDetector.detect(video, gotResult);
+        
+        for (i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are: "+objects.length;
+
+            fill("#FF0000");
+            percent = Math.floor(objects[i].confidence * 100);
+            text(objects[i].label + " "+percent +"%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
 
 function start()
@@ -30,4 +49,15 @@ function modelLoaded()
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+
+    console.log(results);
+    objects = results;
 }
